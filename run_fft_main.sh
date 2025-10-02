3b85b64b316d1395e84c24e3ebba5ee01167e7d5
Xin (Lucas) Yu <xmy5152@psu.edu>
2025-10-02 15:55:43 -0400

    Update run_fft_main.sh

--- run_fft_main.sh
+++ run_fft_main.sh
@@ -14,15 +14,20 @@ get_free_port() {
 MASTER_PORT=$(get_free_port)
 echo "Using free port: $MASTER_PORT"
 
-accelerate launch \
+output_dir=./checkpoints/fullft-llama3-8b
+log_out=train_$(date +%Y%m%d_%H%M%S).log
+mkdir -p $output_dir
+
+nohup accelerate launch \
   --config_file fsdp_config.yaml \
   --main_process_port $MASTER_PORT \
   main_fft.py \
   --model_name_or_path meta-llama/Meta-Llama-3-8B \
   --dataset_name HuggingFaceH4/Code-Feedback \
-  --output_dir ./checkpoints/fullft-llama3-8b \
+  --output_dir $output_dir \
   --per_device_train_batch_size 2 \
   --per_device_eval_batch_size 1 \
-  --max_steps 1000 \
-  --learning_rate 1e-6 \
-  --gradient_checkpointing True
+  --max_steps 10000 \
+  --learning_rate 1e-7 \
+  --gradient_checkpointing True \
+  > ${output_dir}/${log_out} 2>&1 &
